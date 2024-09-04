ed0540f805dc5e917499e904d5cafb40833ade13
Pablo Miguel <105248505+1mperador@users.noreply.github.com>
2024-09-03 22:19:07 -0300

    Como fazer redirecionamento.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import Titulo from "./Titulo"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
 
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes> {/*Routes: Sistema de Rotas ou links */}
+          <Route path="/" element={<Home />}/>{/*path: nome do caminho element: vai ser rederizado depois do path */}
+          <Route path="/sobre" element={<Sobre />}/>
+          <Route path="/contato" element={<Contato />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+{/*
+import Titulo from "./Titulo"
 function App(){
   return (
     <div>
-       <Titulo cor="red" /> {/*Props é somente leitura*/}
+       <Titulo cor="red" /> Props é somente leitura
        <Titulo cor="blue" />
        <Titulo cor="purple" />
        <Titulo cor="orangered"/> 
@@ -12,5 +42,4 @@ function App(){
   
  
 }
-
-export default App
+ */}
